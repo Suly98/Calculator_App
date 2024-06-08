f82f853e7d75b98f828914a55616f751287d18e2
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-08 13:31:15 +0400

    added addition test function

--- src/main/java/com/Calculator/App/Service/CalculatorServiceTest.java
+++ src/main/java/com/Calculator/App/Service/CalculatorServiceTest.java
@@ -4,4 +4,8 @@ public class CalculatorServiceTest {
     private final CalculatorService calculatorService = new CalculatorService();
 
 
+    public void testAddition(){
+        assertEquals("Result of 5.0 - 3.0 = 2.0", calculatorService.getAnswer(5.0,"-", 3.0));
+
+    }
 }
