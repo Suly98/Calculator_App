57d11e1446939d819ef690c3bccd0190fdfa1213
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-08 13:32:10 +0400

    added JUnit testing dependency

--- src/main/java/com/Calculator/App/Service/CalculatorServiceTest.java
+++ src/main/java/com/Calculator/App/Service/CalculatorServiceTest.java
@@ -1,9 +1,12 @@
 package com.Calculator.App.Service;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorServiceTest {
     private final CalculatorService calculatorService = new CalculatorService();
 
-
+    @Test
     public void testAddition(){
         assertEquals("Result of 5.0 - 3.0 = 2.0", calculatorService.getAnswer(5.0,"-", 3.0));
 
