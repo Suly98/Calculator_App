9fcba27dccce7ce255505e7faafe2c5b1556b766
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-08 12:03:25 +0400

    created isValidOperator

--- src/main/java/com/Calculator/App/Service/ValidationService.java
+++ src/main/java/com/Calculator/App/Service/ValidationService.java
@@ -1,4 +1,10 @@
 package com.Calculator.App.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ValidationService {
+    public Boolean isValidOperator(Double num1, Double num2){
+
+    }
 }
