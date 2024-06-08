148668e2c0a74688ff8f237e7d20984530e2b7c4
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-08 12:06:22 +0400

    added the operators

--- src/main/java/com/Calculator/App/Service/ValidationService.java
+++ src/main/java/com/Calculator/App/Service/ValidationService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class ValidationService {
     public Boolean isValidOperator(String operator){
-
+        return operator.matches("[+/*%-^]");
     }
 }
