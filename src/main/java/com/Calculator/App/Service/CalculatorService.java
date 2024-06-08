a8e78f5873d91b2de9e4691d34b2479056c1accc
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-08 10:23:21 +0400

    created CalculatorService

---
+++ src/main/java/com/Calculator/App/Service/CalculatorService.java
@@ -0,0 +1,12 @@
+package com.Calculator.App.Service;
+
+import org.springframework.stereotype.Service;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+@Service
+public class CalculatorService {
+
+
+}
